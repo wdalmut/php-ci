158787e3be063526788df9b8c7b24030ce617a96
Walter Dal Mut <walter.dalmut@gmail.com>
2011-11-30 22:53:31 +0100

    Minor fix

--- tests/1/1.1.php
+++ tests/1/1.1.php
@@ -8,6 +8,9 @@ echo "Lisa Rose vs Gene Seymour -> " . $distance . PHP_EOL;
 $distance = sim_distance($critics["Michael Phillips"], $critics["Claudia Puig"]);
 echo "Michael Phillips vs Claudia Puig -> " . $distance . PHP_EOL;
 
+$distance = sim_distance($critics["Michael Phillips"], $critics["Gene Seymour"]);
+echo "Michael Phillips vs Gene Seymour -> " . $distance . PHP_EOL;
+
 function sim_distance($person1, $person2)
 {
     $keys = array_keys(
@@ -24,10 +27,12 @@ function sim_distance($person1, $person2)
         $first = implode(",", $person1);
         $second = implode(",", $person2);
         
-        $point = pow(($person1[$value]-$person2[$value]), 2);
-        $points[] = $point;
-        
-        $distance += $point;
+        if (@$person1[$value] && @$person2[$value]) {
+            $point = pow(($person1[$value]-$person2[$value]), 2);
+            $points[] = $point;
+            
+            $distance += $point;
+        }
     }
     
     $distance = 1/(1+$distance);
