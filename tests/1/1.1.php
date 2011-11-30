5ffc08e6378c0c1d35708ac50a62d153cdbc26ac
Walter Dal Mut <walter.dalmut@gmail.com>
2011-11-30 21:45:05 +0100

    Added base Eucli...

---
+++ tests/1/1.1.php
@@ -0,0 +1,29 @@
+<?php 
+
+$critics = include '../dataset.php';
+
+$distance = sim_distance($critics["Lisa Rose"], $critics["Gene Seymour"]);
+echo $distance . PHP_EOL;
+
+$distance = sim_distance($critics["Michael Phillips"], $critics["Claudia Puig"]);
+echo $distance . PHP_EOL;
+
+//TODO: refactor
+function sim_distance($person1, $person2)
+{
+    $keys = array_keys(
+        array_merge(
+            $person1, 
+            $person2
+        )
+    );
+    
+    $distance = 0;
+    foreach ($keys as $value) {
+        $distance += pow(($person1[$value]-$person2[$value]), 2);
+    }
+    
+    $distance = 1/(1+$distance);
+    
+    return $distance;
+}
---
+++ tests/dataset.php
@@ -0,0 +1,35 @@
+<?php 
+return array(
+    'Lisa Rose' => array(
+        'Lady in the Water' => 2.5,
+        'Snakes on a Plane' => 3.5,
+        'Just my Luck' => 3.0,
+        'Superman Returns' => 3.5,
+        'You, Me and Dupree' => 2.5,
+        'The Night Listener' => 3.0
+    ),
+    'Gene Seymour' => array(
+		'Lady in the Water' => 3.0,
+        'Snakes on a Plane' => 3.5,
+        'Just my Luck' => 1.5,
+        'Superman Returns' => 5.0,
+        'You, Me and Dupree' => 3.0,
+        'The Night Listener' => 3.5
+    ),
+	'Michael Phillips' => array(
+		'Lady in the Water' => 3.0,
+        'Snakes on a Plane' => 3.5,
+        'Just my Luck' => 1.5,
+        'Superman Returns' => 5.0,
+        'You, Me and Dupree' => 3.0,
+        'The Night Listener' => 3.5
+    ),
+	'Claudia Puig' => array(
+		'Lady in the Water' => 3.0,
+        'Snakes on a Plane' => 3.5,
+        'Just my Luck' => 1.5,
+        'Superman Returns' => 5.0,
+        'You, Me and Dupree' => 3.0,
+        'The Night Listener' => 3.5
+    )
+);
