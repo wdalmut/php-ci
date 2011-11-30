1f68ca826de9b0f4ce2908e0259f616ff3ff9442
Walter Dal Mut <walter.dalmut@gmail.com>
2011-11-30 22:53:39 +0100

    Minor on sets

--- tests/dataset.php
+++ tests/dataset.php
@@ -13,23 +13,20 @@ return array(
         'Snakes on a Plane' => 3.5,
         'Just my Luck' => 1.5,
         'Superman Returns' => 5.0,
-        'You, Me and Dupree' => 3.0,
-        'The Night Listener' => 3.5
+        'The Night Listener' => 3.0,
+		'You, Me and Dupree' => 3.5
     ),
 	'Michael Phillips' => array(
-		'Lady in the Water' => 3.0,
-        'Snakes on a Plane' => 3.5,
-        'Just my Luck' => 1.5,
-        'Superman Returns' => 5.0,
-        'You, Me and Dupree' => 3.0,
-        'The Night Listener' => 3.5
+		'Lady in the Water' => 2.5,
+        'Snakes on a Plane' => 3.0,
+        'Superman Returns' => 3.5,
+        'The Night Listener' => 4.0
     ),
 	'Claudia Puig' => array(
-		'Lady in the Water' => 3.0,
         'Snakes on a Plane' => 3.5,
-        'Just my Luck' => 1.5,
-        'Superman Returns' => 5.0,
-        'You, Me and Dupree' => 3.0,
-        'The Night Listener' => 3.5
+        'Just my Luck' => 3.0,
+        'You, Me and Dupree' => 2.5,
+		'Superman Returns' => 4.0,
+        'The Night Listener' => 4.5
     )
 );
